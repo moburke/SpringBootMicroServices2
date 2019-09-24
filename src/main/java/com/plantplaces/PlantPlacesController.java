144de54eda1b5b3b83f9633b734da51ef469d0e3
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-24 15:10:46 -0400

    Testing using Postman - going through the url using breakpoints

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,9 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PlantPlacesController {
@@ -10,11 +12,31 @@ public class PlantPlacesController {
 	 * Handle the /start endpoint.
 	 * @return
 	 */
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
 		
 		return "start";
 	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET,headers= {"content-type=text/json"})
+	public String readJSON() {
+		
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue() {
+		
+		return "start";
+	}
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public String readSilver() {
+		
+		return "start";
+	}
+	
+	
 	
 	/**
 	 * Handle the / endpoint
@@ -22,6 +44,11 @@ public class PlantPlacesController {
 	 */
 	@RequestMapping("/")
 	public String index() {
+		
+		return "start";
+	}
+	@PostMapping("/start")
+	public String create() {
 		
 		return "start";
 	}
